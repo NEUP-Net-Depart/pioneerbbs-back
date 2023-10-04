e8678b7348b8ea1a34957019eba4b72397b3557f
PermissionDog <455662503@qq.com>
2023-10-04 16:48:16 +0800

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/wait", func(c *gin.Context) {
+		c.JSON(200, map[string]interface{}{
+			"notification": "wait",
+		})
+	})
+	r.Run(":19198")
+}
